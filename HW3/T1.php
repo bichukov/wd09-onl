da71a5cd2c523b696bfbb20d018e2419a8152c7b
Seregadixi <seregadixi@mail.ru>
2023-01-18 23:00:47 +0300

    HW3

--- HW3/T1.php
+++ HW3/T1.php
@@ -1,1 +1,15 @@
 <?php
+$a=rand(1,100);
+function detect_number($a)
+{
+    if($a % 2 == 0) //определяем что число четное (остаток от деления на 2 = 0)
+    {
+        echo "$a - Четное " ;
+    }
+    else
+    {
+        return ("Не четное - $a");
+    }
+}
+echo detect_number($a);
+
--- HW3/T2.php
+++ HW3/T2.php
@@ -1,1 +1,12 @@
 <?php
+$d = rand(1, 30);
+$m = rand(1, 12);
+$y = rand(1, 2400);
+$timestamp = strtotime("$d-$m-$y");
+
+if (idate('L', $timestamp) == 1) {
+    print_r("$d-$m-$y является високосным");
+} else {
+    print_r("$d-$m-$y является не високосным");
+}
+?>
