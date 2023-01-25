99a46c75ff7de5a71f47eee6863835ac294d2b01
Seregadixi <seregadixi@mail.ru>
2023-01-26 01:33:07 +0300

    HW6

--- HW6/T1.php
+++ HW6/T1.php
@@ -1,27 +1,20 @@
 <?php
-//Определить по восточному календарю кто Вы по знаку зодиака без использования конструкции if и массивов.
-$day = "22.01";
-$bDay = strtotime($day);
-$sd=match (true){
-    strtotime(21.03)<=$bDay && strtotime(19.04)>=$bDay=>"Вы Овен",
-    strtotime(20.04)<=$bDay && strtotime(21.05)>=$bDay=>"Вы Телец",
-    strtotime(21.05)<=$bDay && strtotime(20.06)>=$bDay=>"Вы Близнецы",
-    strtotime(21.06)<=$bDay && strtotime(22.07)>=$bDay=>"Вы Рак",
-    strtotime(23.07)<=$bDay && strtotime(22.08)>=$bDay=>"Вы Лев",
-    strtotime(23.08)<=$bDay && strtotime(22.09)>=$bDay=>"Вы Дева",
-    strtotime(23.09)<=$bDay && strtotime(22.10)>=$bDay=>"Вы Весы",
-    strtotime(23.10)<=$bDay && strtotime(21.11)>=$bDay=>"Вы Скорпион",
-    strtotime(22.11)<=$bDay && strtotime(21.12)>=$bDay=>"Вы Стрелец",
-    strtotime(22.12)>=$bDay && strtotime(19.01)>=$bDay=>"Вы Козерог",
-    strtotime(20.01)<=$bDay && strtotime(18.02)>=$bDay=>"Вы Водолей",
-    default=>"Вы Рыбы"
-};
-print_r($sd . "\n") ;
+//Проверка на белорусские автомобильные номера
+$console = $argv[1] . $argv[2];
+$regularNumber = '/[0-9]{4}\s?[A-Za-z]{2}-[0-7]/i';
+$transitNumber = '/[0-9][A-Za-z]{2}[0-9]{4}/i';
+$electronNumber = '/E[0-9]{3}\s?[A-Za-z]{2}-[0-7]/i';
+$regularNumberTrue = preg_match($regularNumber, $console);
+$isElectricNumberTrue = preg_match($electronNumber, $console);
+$isTransitNumberTrue = preg_match($transitNumber, $console);
+if ($regularNumberTrue == 1) {
+    echo 'Номер ' . $console . ' является обычным белорусским номером';
+} elseif ($isElectricNumberTrue == 1) {
+    echo 'Номер ' . $console . ' является белорусским номером электромобиля';
+} elseif ($isTransitNumberTrue == 1) {
+    echo 'Номер ' . $console . ' является белорусским транзитным номером';
+} else {
+    echo 'Номер ' . $console . ' введен не верно или не является белорусским номером';
+}
 
-//не понимаю как правильно оформить чтобы он работал как надо
-
-
-
-
-
-
+//Пробовал сделать не через if а через match но не вышло. Не смог разобраться как через него это реализовать.
