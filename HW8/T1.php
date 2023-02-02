19815e46bbeb72e406b2f32a247b659a257a222a
Seregadixi <seregadixi@mail.ru>
2023-02-02 13:49:57 +0300

    HW8

--- HW8/T1.php
+++ HW8/T1.php
@@ -1,20 +1,97 @@
 <?php
-//Проверка на белорусские автомобильные номера
-$console = $argv[1] . $argv[2];
-$regularNumber = '/[0-9]{4}\s?[A-Za-z]{2}-[0-7]/i';
-$transitNumber = '/[0-9][A-Za-z]{2}[0-9]{4}/i';
-$electronNumber = '/E[0-9]{3}\s?[A-Za-z]{2}-[0-7]/i';
-$regularNumberTrue = preg_match($regularNumber, $console);
-$isElectricNumberTrue = preg_match($electronNumber, $console);
-$isTransitNumberTrue = preg_match($transitNumber, $console);
-if ($regularNumberTrue == 1) {
-    echo 'Номер ' . $console . ' является обычным белорусским номером';
-} elseif ($isElectricNumberTrue == 1) {
-    echo 'Номер ' . $console . ' является белорусским номером электромобиля';
-} elseif ($isTransitNumberTrue == 1) {
-    echo 'Номер ' . $console . ' является белорусским транзитным номером';
+error_reporting(0);
+$peoples = file("peoples.csv");
+
+$data = [];
+foreach ($peoples as $line) {
+    $data[] = str_getcsv($line);
+}
+//$data = array_map('str_getcsv', file('data.csv'));
+
+if (!empty($_POST['country'])) {
+    $searchPeoples = [];
+    foreach ($data as $people) {
+        if ($people[4] == $_POST['country']) {
+            $searchPeoples[] = $people;
+        }
+    }
+} elseif (empty($_POST['country']) && !empty($_POST['name'])) {
+
+    $searchPeoples = [];
+    $sd = "";
+    foreach ($data as $people) {
+        $sd = preg_match("/(" . $_POST['name'] . ".*?)/i", $people[0]);
+        if ($_POST['name-check'] == '') {
+            if ($people[0] == $_POST['name'] || $sd == 1) {
+                $searchPeoples[] = $people;
+            }
+
+        } else {
+            if ($people[0] == $_POST['name']) {
+                $searchPeoples[] = $people;
+            }
+        }
+    }
+
 } else {
-    echo 'Номер ' . $console . ' введен не верно или не является белорусским номером';
+    $searchPeoples = $data;
 }
 
-//Пробовал сделать не через if а через match но не вышло. Не смог разобраться как через него это реализовать.
+
+////Проверка на белорусские автомобильные номера
+//
+//$dff=preg_match("/(Клар.*?)/i",$console);
+//
+//if ($dff==1){ echo "ghh";}
+//?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Codes</title>
+</head>
+<body>
+<div class="container">
+    <form action="" method="POST">
+        <div class="form-group">
+            <label>Enter country</label>
+            <input type="text" name="country" class="form-control" placeholder="Enter country">
+        </div>
+        <div class="form-group">
+            <label>Enter name</label>
+            <input type="text" name="name" class="form-control" placeholder="Enter country">
+            <input type="checkbox" name="name-check" class="form-controller" value="1">Полное совпадение
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">Address</th>
+            <th scope="col">Country</th>
+            <th scope="col">Region</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($searchPeoples as $people) : ?>
+            <tr>
+                <th scope="row"><?= $people[0] ?></th>
+                <td><?= $people[1] ?></td>
+                <td><?= $people[2] ?></td>
+                <td><?= $people[3] ?></td>
+                <td><?= $people[4] ?></td>
+                <td><?= $people[5] ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
